5b4f166802d51e1de3365cb7dcb5ddf6c5a7a02f
wael ogua <waelogua@gmail.com>
2020-06-06 21:09:08 +0300

    TEST

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 style={{color:"red"}}>Coronavirus Disease (COVID-19) Dashboard Live.</h1>
+     <body style={{backGroundcolor: "red"}}>
+  <div class="boxex" style={{display: "flex"}}>
+  <div class="box">
+    <h1 style={{color: "red" ,textAlign: "center", fontFamily: "serif"}}> Global Statistics</h1>
+    <h2 id="totalC" style={{fontFamily: "serif",color:"white"}}>
+    <h3 id="myDiv"></h3>
+    </h2>
+  </div>
+  <div class="box2">
+    <h1 style={{color: "red" ,textAlign: "center", fontFamily: "serif"}}>Ukraine Statistics</h1>
+    <h2 id="ukraine" style={{fontFamily: "serif",color:"red"}}>
+    <h3 id="myDiv"></h3>
+    </h2>
+  </div>
+</div>
+    <h1 style={{color: "red",margin:"20px",textAlign: "left"}}>Statistics by country :</h1>
+    <table style={{backgroundColor: "red",margin: "20px"}}>
+        <tr bgcolor="red" style={{color:"blue", fontSize: "larger"}}>
+          <th>Country</th>
+          <th>New confirmed</th>
+          <th><span id="color" style={{color: "red"}}>New deaths</span></th>
+          <th>New recovered</th>
+          <th>Total confirmed</th>
+          <th>Total deaths</th>
+          <th>Total recovered </th>
+        </tr>
+        <tbody class="table table-striped table-dark" id="country-list"></tbody>
+      </table>
+      <canvas id="myChart"></canvas>
+      
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/gsap.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+      <script src="app.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+      <script src="path/to/chartjs/dist/Chart.js"></script>
+</body>
     </div>
+  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,82 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import $ from 'jquery' ;
+import Chart from 'chart.js';
+
+
+let ctx = 'myChart';
+let myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+  }
+});
+//fetch data
+$.getJSON("https://api.covid19api.com/summary",function(data){
+  console.log(data);
+  console.log(data.Countries[174]);
+  console.log(data.Countries);
+
+   // Set ukraine statistics
+    $("#ukraine").append(
+    "<h2>" + "New Confirmed "+ "</h2>" +"<h3>"+data.Countries[174].NewConfirmed+ "</h3>"+
+    "<h2>" + "Total Confimed "+ "</h2>" +"<h3>"+ data.Countries[174].TotalConfirmed+ "</h3>"+
+    "<h2>" + "New Deaths "+ "</h2>" +"<h3>"+ data.Countries[174].NewDeaths+ "</h3>"+
+    "<h2>" + "Total Deaths "+ "</h2>" +"<h3>"+ data.Countries[174].TotalDeaths+ "</h3>"+
+    "<h2>" + "New Recovered "+ "</h2>" +"<h3>"+ data.Countries[174].NewRecovered+ "</h3>"+
+    "<h2>" + "Total Recovered "+ "</h2>" +"<h3>"+ data.Countries[174].TotalRecovered+ "</h3>",
+
+    );
+    
+  // Global statistics
+  $.each(data.Global, function(key,value){
+    $("#totalC").append("<h2>" + key + "<h3>" +  value + "</h3>"+ "</h2>");
+    
+  });
+  
+  // Stats by country
+$.each(data.Countries,function(key,value){
+  $('#country-list').append(`<tr><td>${value.Country}</td><td>${value.NewConfirmed}</td><td>${value.NewDeaths}</td><td>${value.NewRecovered}</td><td>${value.TotalConfirmed}</td><td>${value.TotalDeaths}</td><td>${value.TotalRecovered}</td></tr>`);
+})
+
+// Date
+$('.title').append("<h1>" + data.Date +"</h1>")
+
+}
+
+
+);
 
 ReactDOM.render(
   <React.StrictMode>
