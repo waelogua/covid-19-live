b44929cc4babe55edd6b240792330ef0879f75e4
wael ogua <waelogua@gmail.com>
2020-06-06 22:15:10 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
     <h1 style={{color:"red"}}>Coronavirus Disease (COVID-19) Dashboard Live.</h1>
-     <body style={{backGroundcolor: "red"}}>
+     <body style={{color: "red"}}>
   <div class="boxex" style={{display: "flex"}}>
   <div class="box">
     <h1 style={{color: "red" ,textAlign: "center", fontFamily: "serif"}}> Global Statistics</h1>
@@ -39,7 +39,7 @@ function App() {
       <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
       <script src="app.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-      <script src="path/to/chartjs/dist/Chart.js"></script>
+      
 </body>
     </div>
   
